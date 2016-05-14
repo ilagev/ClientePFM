75bfc20b08ac03b484801cac0350b72cb5f55ba1
Iván <ilagev@gmail.com>
2016-05-15 01:11:43 +0200

    Searching smartphones (very basic) working

--- public_html/app/controllers/searchController.js
+++ public_html/app/controllers/searchController.js
@@ -1,9 +1,15 @@
 angular.module('sc').controller('searchController',
-    function($scope, $http) {
+    function($scope, $http, RESOURCES) {
         $scope.name = "";
         
         $scope.search = function () {
             console.log("Model name = " + $scope.name);
+            
+            var url = RESOURCES.BASE + RESOURCES.SMARTPHONES + "?query=" + $scope.name;
+            $http.get(url)
+                .then(function(response) {
+                    console.log(response.data);
+                });
         };
     }
 );
